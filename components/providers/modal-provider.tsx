58f2c345f92179e203b66add8cde9afc65ee112a
mhshifat <mehedihassanshifat01@gmail.com>
2023-09-13 19:06:33 +0600

    delete server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import ClientOnly from "../client-only";
 import CreateChannelModal from "../modals/create-channel-modal";
 import CreateServerModal from "../modals/create-server-modal";
+import DeleteServerModal from "../modals/delete-server-modal";
 import EditServerModal from "../modals/edit-server-modal";
 import InviteModal from "../modals/invite-modal";
 import LeaveServerModal from "../modals/leave-server-modal";
@@ -17,6 +18,7 @@ export default function ModalProvider() {
       <MembersModal />
       <CreateChannelModal />
       <LeaveServerModal />
+      <DeleteServerModal />
     </ClientOnly>
   )
 }
