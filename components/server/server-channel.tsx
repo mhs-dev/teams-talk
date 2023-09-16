784dd88fb75434eba0821caaa00fe062858f6c61
mhshifat <mehedihassanshifat01@gmail.com>
2023-09-16 13:58:31 +0600

    edit channel and delete channel

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -5,10 +5,12 @@ import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import ActionTooltip from "../action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
+import { ServerWithMembersWithProfiles } from "@/types";
 
 interface ServerChannelProps {
   channel: Channel;
-  server: Server;
+  server: ServerWithMembersWithProfiles;
   role?: MemberRole;
 }
 
@@ -19,6 +21,7 @@ const iconMap = {
 }
 
 export default function ServerChannel({ channel, server, role }: ServerChannelProps) {
+  const { onOpen } = useModal();
   const params = useParams();
   const router = useRouter();
 
@@ -40,10 +43,10 @@ export default function ServerChannel({ channel, server, role }: ServerChannelPr
       {channel.name !== 'general' && role !== MemberRole.GUEST && (
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label="Edit">
-            <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            <Edit onClick={() => onOpen("editChannel", { server, channel })} className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
           <ActionTooltip label="Delete">
-            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            <Trash onClick={() => onOpen("deleteChannel", { server, channel })} className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
         </div>
       )}
