ebb58ad565f2a2769206b84e33cea4628f192fca
mhshifat <mehedihassanshifat01@gmail.com>
2023-09-17 14:23:33 +0600

    rendered messages

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from "@/components/chat/chat-header";
 import ChatInput from "@/components/chat/chat-input";
+import ChatMessages from "@/components/chat/chat-messages";
 import currentProfile from "@/lib/current-profile"
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -19,9 +20,21 @@ export default async function ChannelPage({ params }: { params: { serverId: stri
         serverId={channel.serverId}
         type='channel'
       />
-      <div className="flex-1">
-        {/*  */}
-      </div>
+      
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type="channel"
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId
+        }}
+        paramKey="channelId"
+        paramValue={channel.id}
+      />
 
       <ChatInput
         name={channel.name}
